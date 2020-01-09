70bfe6bbcdb5aa0bb38d6393acbbcd9b46b9e7ae
es <sandstrom.ester@gmail.com>
2020-01-09 13:55:53 +0100

    Initial commit

---
+++ QuizProject/BoardPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace QuizProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BoardPage : Page
+    {
+        public BoardPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RollDie(object sender, RoutedEventArgs e)
+        {
+            if (die.Text == "a")
+            {
+                die.Text = "b";
+            }
+            else
+            {
+                die.Text = "a";
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# QuizProject
+
+- Detta är ett frågesportsspel
+- I början av projektet krånglar Lovedeeps dator och vi parprogrammerar, de commitsen har namn som börjar med Ester och Lovedeep.
